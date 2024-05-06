61ae0d6b60483b0d5c6bfd83a548fce4c64e5d41
yaohengyu <1798336309@qq.com>
2024-05-06 09:07:10 +0800

    update website router

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.get('/website/**', function(req, res, next) {
+  res.sendFile( path.join(__dirname, '/public') + "/" + "index.html" )
+});
 // app.get(/.*/,function(req,res){
 //   res.sendFile(__dirname+'../public/')})
 
